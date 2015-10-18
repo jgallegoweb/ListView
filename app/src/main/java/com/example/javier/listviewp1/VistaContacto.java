f1aa7b2afcbd9aa272754bf0f02405cabfad8896
Javier Gallego <jgallegoweb@gmail.com>
2015-10-18 17:50:45 +0200

    0.9.1 Diseño final

--- app/src/main/java/com/example/javier/listviewp1/VistaContacto.java
+++ app/src/main/java/com/example/javier/listviewp1/VistaContacto.java
@@ -26,25 +26,6 @@ public class VistaContacto extends AppCompatActivity {
         init();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_vista_contacto, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     private void init(){
         Bundle p = getIntent().getExtras();
         contacto = (Contacto)p.getSerializable("contacto");
